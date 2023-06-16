86f971077af6c3e48fd0ea582277dbf7e02309ef
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-06-16 11:55:20 -0300

    Criando a primeira connection e channel

---
+++ rabbitmq/src/main/java/com/rabbitmq/example/rabbitmq/Publisher.java
@@ -0,0 +1,23 @@
+package com.rabbitmq.example.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Publisher {
+    public Publisher() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        String message = "First message";
+
+        channel.basicPublish("", "Queue-1", null, message.getBytes());
+
+        channel.close();
+        connection.close();
+    }
+}
